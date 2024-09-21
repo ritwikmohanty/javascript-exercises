20abc24554f13fb247e140d9968c9efa54c9ce4b
ritwikmohanty <ritwikmohanty3900@gmail.com>
2024-09-22 00:49:43 +0530

    Solved first three questions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
-};
+const repeatString = function(a,b) {
+    if(b>=0){
+ return a.repeat(b);
+}else{
+    return "ERROR"
+}};
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(a) {
+    n=a.length;word='';
+    for(i=n-1;i>=0;i--){
+        word= word+a.charAt(i);
+    }
+    return word;
 };
 
 // Do not edit below this line
