6de0fa2144c42e27b0b9f6f71a5eb1b963cf1330
ritwikmohanty <ritwikmohanty3900@gmail.com>
2024-09-26 19:38:03 +0530

    Solve 5th

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum=0,i;
+    if(typeof a=='number' && typeof b=='number' && a%1==0 && b%1==0 && a>=0 && b>=0){
+    if(a<b){
+     for(i=a;i<=b;i++){
+        sum=sum+i;
+     }}
+     else{
+        for(i=b;i<=a;i++){
+            sum=sum+i;
+     }}
+     return sum;}
+     else{
+      return "ERROR"
+     }
 };
 
 // Do not edit below this line
